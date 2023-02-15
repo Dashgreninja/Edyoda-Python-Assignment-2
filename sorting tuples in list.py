3224dcee4c0e1946f45ef51f681077bdb1a939f1
Dashgreninja <68371315+Dashgreninja@users.noreply.github.com>
2023-02-15 11:47:59 +0530

    Update sorting tuples in list.py

--- sorting tuples in list.py
+++ sorting tuples in list.py
@@ -1,2 +1,7 @@
+def index(n):  
+    return n[1]    
+
 l=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-print(sorted(l, key=lambda a:a[1]))
+  
+print("Sorted list of Tuples:"),  
+print(sorted(l, key = index))
